33a6c3b73f3bb3c924fbdcd24938ffa5c492e93c
Fragonite <@>
2024-03-10 17:58:44 +0800

    Update oldunreal bash script

--- dump_oldunreal.sh
+++ dump_oldunreal.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # https://github.com/OldUnreal/UnrealTournamentPatches/releases
-wget https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v469d-rc2/OldUnreal-UTPatch469d-Linux-amd64.tar.bz2
-tar -xvf OldUnreal-UTPatch469d-Linux-amd64.tar.bz2 -C tmp/oldunreal
-mv tmp/oldunreal .
+# wget https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v469d/OldUnreal-UTPatch469d-Linux-arm64.tar.bz2 -O OldUnreal.tar.bz2
+wget https://github.com/OldUnreal/UnrealTournamentPatches/releases/download/v469d/OldUnreal-UTPatch469d-Linux-amd64.tar.bz2 -O OldUnreal.tar.bz2
+mkdir tmp-oldunreal
+mkdir tmp-oldunreal/oldunreal
+mkdir tmp/oldunreal
+tar -xvf OldUnreal.tar.bz2 -C tmp-oldunreal/oldunreal
+mv tmtmp-oldunrealp/oldunreal .
